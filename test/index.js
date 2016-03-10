ab8663da8a113c128d7abe599e9cae58f4b329c5
DAgutierrez <diego.gutierrez684@gmail.com>
2016-03-10 16:29:19 -0300

    init project

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import es6Fundamentals from '../lib';
+
+describe('es-6-fundamentals', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
